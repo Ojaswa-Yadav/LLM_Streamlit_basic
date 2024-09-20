5ecf32e13ce690c53212a46e19dcf46d5c51f676
Ojaswa <71908842+Ojaswa-Yadav@users.noreply.github.com>
2024-09-19 20:32:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-# Setting up the title for your app
-st.title("LLM-App 🧠")
-
-# API key input field
+# Add your OpenAI API Key
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
-# Function to generate a response from the OpenAI model
+# Function to generate responses
 def generate_response(input_text):
-    # Initializing the OpenAI model with the provided API key
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    # Generating and displaying the response
     response = llm(input_text)
     st.info(response)
 
-# Input text box for the user to enter a prompt
+# Text area for user input
 input_text = st.text_area("Enter your prompt here:")
 
-# Button to trigger the response generation
+# Generate response when button is clicked
 if st.button("Generate Response"):
     if openai_api_key:
         if input_text:
@@ -27,4 +22,3 @@ if st.button("Generate Response"):
             st.warning("Please enter a prompt.")
     else:
         st.warning("Please enter your OpenAI API Key.")
-
